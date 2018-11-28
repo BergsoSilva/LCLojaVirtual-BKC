65dfcb121e36f3eb3a110bb559f6e9fb134237ca
Bergson <bergson.keren.silva@gmail.com>
2018-11-28 17:29:36 -0400

    Erro de ortografia

--- src/main/java/com/silva/lacoscomfitaApp/Resource/ClienteResource.java
+++ src/main/java/com/silva/lacoscomfitaApp/Resource/ClienteResource.java
@@ -4,7 +4,6 @@ package com.silva.lacoscomfitaApp.Resource;
 
 
 import com.silva.lacoscomfitaApp.model.Cliente;
-import com.silva.lacoscomfitaApp.repository.ClienteRoposiory;
 import io.swagger.annotations.Api;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.silva.lacoscomfitaApp.repository.ClienteRepository;
 
 /**
  *
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClienteResource {
 
     @Autowired
-    ClienteRoposiory clienteService;
+    ClienteRepository clienteService;
 
     @GetMapping
     public List<Cliente> listar(){
